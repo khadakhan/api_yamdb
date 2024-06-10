9fd29a55c68e0e646248978306bff0296354ae2d
Konstantin Khadakhanov <k-project@mail.ru>
2024-06-10 18:36:20 +0300

    Add serializers and views for Review, Comment

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from reviews.models import Comment, Review
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Review
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Comment
+        read_only_fields = ('rewiew',)
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import (CommentSerializer,
+                          ReviewSerializer)
+
+from reviews.models import Review
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+
+    def get_review(self):
+        return get_object_or_404(Review, id=self.kwargs.get('review_id'))
+
+    def get_queryset(self):
+        return self.get_review().comments.all()
+
+    def perform_create(self, serializer):
+        serializer.save(post=self.get_review(), author=self.request.user)
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
