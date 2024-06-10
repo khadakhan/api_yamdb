1adb34e752a3bd2a489465332cc44747016a094a
TemkengGitLab <stephentemkeng@gmail.com>
2024-06-10 21:41:52 +0700

    Added admin, migrations, router situation

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -4,11 +4,16 @@ from .views import UserViewSet, SignUpView, MyTokenObtainPairView
 
 app_name = 'api'
 
-v1_router = SimpleRouter()
-v1_router.register('users', UserViewSet, basename='users')
+router = SimpleRouter()
+
+router.register('users', UserViewSet, basename='users')
+
+url_v1 = [
+    path('', include(router.urls)),
+]
 
 urlpatterns = [
-    path('v1/', include(v1_router.urls)),
+    path('v1/', include(url_v1)),
     path('v1/auth/signup/', SignUpView.as_view(), name='signup'),
     path('v1/auth/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
