d254566c4ef68909e1d1a4f3d7c0e7ecac5eae0e
Konstantin Khadakhanov <k-project@mail.ru>
2024-06-10 15:56:32 +0300

    Models Comment Rewiew

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,36 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Review(models.Model):
+    title = models.IntegerField()  # Заглушка для ForeingKey(Title, )
+    text = models.TextField()
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='rewiews'
+    )
+    score = models.IntegerField(
+        default=5,
+        validators=[MaxValueValidator(10), MinValueValidator(1)]
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации отзыва',
+        auto_now_add=True
+    )
+
+
+class Comment(models.Model):
+    rewiew = models.ForeignKey(
+        Review, on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    text = models.TextField()
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments'
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации комментария',
+        auto_now_add=True
+    )
