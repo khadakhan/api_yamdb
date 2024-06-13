c83f0d53f76ca7b3ed12e94147b591b3a8133f4b
Konstantin Khadakhanov <k-project@mail.ru>
2024-06-13 15:21:26 +0300

    change 'review_id' to 'review'

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -122,7 +122,7 @@ class CommentViewSet(viewsets.ModelViewSet):
         return self.get_review().comments.all()
 
     def perform_create(self, serializer):
-        serializer.save(review_id=self.get_review(), author=self.request.user)
+        serializer.save(review=self.get_review(), author=self.request.user)
 
    
 class ReviewViewSet(viewsets.ModelViewSet):
