97518661adf6fa79955025085e2c516f713699db
steven <stephentemkeng@gmail.com>
2024-06-14 16:59:45 +0700

    permissions edited and migrations folder removed

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -10,18 +10,17 @@ class ReadOnly(BasePermission):
 class AuthorModeratorAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
-            request.method in permissions.SAFE_METHODS
-            or (request.user.is_authenticated)
+                request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated
         )
 
     def has_object_permission(self, request, view, obj):
-        if (
-            request.method in permissions.SAFE_METHODS
-            or obj.author == request.user
-        ):
-            return True
-        elif request.user.is_moderator or request.user.is_admin:
-            return True
+        return (
+                request.method in permissions.SAFE_METHODS
+                or obj.author == request.user
+                or request.user.is_moderator
+                or request.user.is_admin
+        )
 
 
 class IsAdmin(permissions.BasePermission):
